0228412f34cc94e7d07ae8db66cfb00ff58f93c9
Damon <warrenonedge@gmail.com>
2014-03-24 15:39:44 -0400

    just some addition to the business class.

--- src/Business.java
+++ src/Business.java
@@ -1,4 +1,34 @@
 
-public class Business {
+public abstract class Business {
+	public int busNum;
+	public double totalRec;
+	public double taxDue;
+	
+	public Business(int businessNum, double totalReceipt, double taxesDue){
+	busNum = businessNum;
+	totalRec = totalReceipt;
+	taxDue = taxesDue;
+	}
+	
+	public class Restaurants extends Business {
 
+		public Restaurants(int businessNum, double totalReceipt, double taxesDue) {
+			super(businessNum, totalReceipt, taxesDue);
+		}
+		
+	}
+	public class Hotels extends Business {
+
+		public Hotels(int businessNum, double totalReceipt, double taxesDue) {
+			super(businessNum, totalReceipt, taxesDue);
+		}
+		
+	}
+	public class ConvStore extends Business {
+
+		public ConvStore(int businessNum, double totalReceipt, double taxesDue) {
+			super(businessNum, totalReceipt, taxesDue);
+		}
+		
+	}
 }
