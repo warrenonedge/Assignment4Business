35a39baef035d7d517c031cbc3f4a123d1a36d16
Damon <warrenonedge@gmail.com>
2014-03-26 15:30:28 -0400

    Some changes to template.

--- src/Business.java
+++ src/Business.java
@@ -1,33 +1,53 @@
 
 public abstract class Business {
-	public int busNum;
-	public double totalRec;
-	public double taxDue;
+	protected int busNum;
+	protected double totalRec;
+	protected double taxDue;
 	
-	public Business(int businessNum, double totalReceipt, double taxesDue){
-	busNum = businessNum;
-	totalRec = totalReceipt;
-	taxDue = taxesDue;
+	public Business(int businessNum){
+		busNum = businessNum;
 	}
+	
+	public Business(int businessNum, double totalReceipt){
+		busNum = businessNum;
+		totalRec = totalReceipt;
+	}
+	
+	public abstract String print();
 	
 	public class Restaurants extends Business {
 
 		public Restaurants(int businessNum, double totalReceipt, double taxesDue) {
-			super(businessNum, totalReceipt, taxesDue);
+			super(businessNum, taxesDue);
+		}
+
+		public String print() {
+			// TODO Auto-generated method stub
+			return null;
 		}
 		
 	}
 	public class Hotels extends Business {
 
 		public Hotels(int businessNum, double totalReceipt, double taxesDue) {
-			super(businessNum, totalReceipt, taxesDue);
+			super(businessNum, totalReceipt);
+		}
+
+		public String print() {
+			// TODO Auto-generated method stub
+			return null;
 		}
 		
 	}
 	public class ConvStore extends Business {
 
 		public ConvStore(int businessNum, double totalReceipt, double taxesDue) {
-			super(businessNum, totalReceipt, taxesDue);
+			super(businessNum, totalReceipt);
+		}
+
+		public String print() {
+			// TODO Auto-generated method stub
+			return null;
 		}
 		
 	}
